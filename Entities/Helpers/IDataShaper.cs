27fdbd58c6f3025db4c164b4898b6509a854dc00
abdohosny1 <make.y90@yahoo.com>
2023-09-12 20:20:05 +0300

    Add project files.

---
+++ Entities/Helpers/IDataShaper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Entities.Models;
+using System.Threading.Tasks;
+
+namespace Entities.Helpers
+{
+    public interface IDataShaper<T>
+    {
+        IEnumerable<Entity> ShapeData(IEnumerable<T> entities, string fieldsString);
+        Entity ShapeData(T entity, string fieldsString);
+    }
+}
